34b36d9a645d9e14d182c7e1e73a201d27a7a0a1
JeanThirion <jean.thirion@esial.net>
2022-09-26 08:55:05 +1000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fbx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fbc-crm-melbourne-2022';
+}

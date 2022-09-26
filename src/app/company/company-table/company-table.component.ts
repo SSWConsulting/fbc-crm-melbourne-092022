5d302fd7148a6cf83494d097c650bf5efea8a7e2
lukecookssw <lukecook@ssw.com.au>
2022-09-26 15:23:21 +1000

    finished pt 8

---
+++ src/app/company/company-table/company-table.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { ICompany } from '../icompany';
+
+@Component({
+  selector: 'fbc-company-table',
+  templateUrl: './company-table.component.html',
+  styleUrls: ['./company-table.component.scss']
+})
+export class CompanyTableComponent implements OnInit {
+
+  @Input()
+  companies: ICompany[] = [];
+
+  @Output()
+  deleteButtonClicked: EventEmitter<number> = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  deleteCompany(companyId: number) {
+    this.deleteButtonClicked.emit(companyId);
+  }
+}
